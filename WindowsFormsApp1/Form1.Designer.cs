015dfa866d3ba5c4969c6ce674ba1c6ac1b24d14
natimura <natimura10@gmail.com>
2023-08-13 16:58:28 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.actionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MessageLabel.Location = new System.Drawing.Point(299, 169);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(211, 21);
+            this.MessageLabel.TabIndex = 0;
+            this.MessageLabel.Text = "操作内容を表示します";
+            // 
+            // actionButton
+            // 
+            this.actionButton.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.actionButton.Location = new System.Drawing.Point(198, 169);
+            this.actionButton.Name = "actionButton";
+            this.actionButton.Size = new System.Drawing.Size(75, 45);
+            this.actionButton.TabIndex = 1;
+            this.actionButton.Text = "action";
+            this.actionButton.UseVisualStyleBackColor = true;
+            this.actionButton.Click += new System.EventHandler(this.actionButton_Click);
+            this.actionButton.MouseLeave += new System.EventHandler(this.actionButton_MouseLeave);
+            this.actionButton.MouseHover += new System.EventHandler(this.actionButton_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.actionButton);
+            this.Controls.Add(this.MessageLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.Button actionButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void actionButton_Click(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = "クリックされました";
+        }
+
+
+        private void actionButton_MouseHover(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = "マウスが乗りました";
+        }
+
+        private void actionButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = "マウスがおりました";
+        }
+    }
+}
